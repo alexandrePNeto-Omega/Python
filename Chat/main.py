61ca565f2117563f6aacde951f35765bbe297945
alexandrePNeto-Omega <suporte9@omegaautomacao.com>
2023-07-08 18:28:55 -0300

    *

--- Chat/main.py
+++ Chat/main.py
@@ -2,51 +2,70 @@
 import datetime
 
 ####    Class's
-class Usuario:
-    def __init__(self, nome):
-        self.nome   = nome
+class User:
+    def __init__(self, name):
+        self.name   = name
 
 ####    Functions
 def chatShow():
+    #   Getting first user was sent a msg
+    firstAqr = open("chat.txt", "r")
+    listAqr = firstAqr.readlines()
+    lastUser = str(str(listAqr[0]).split(':')[0])
+
     while True:
         arq = open("chat.txt", "r")
         listMsg = arq.readlines()
         tempList = ['']
         for m in listMsg:
-            if 'envida' not in str(m.replace('\n', '')):
+            formtMsg = str(m.replace('\n', ''))
+            if '&$#@24!%@' not in formtMsg and formtMsg != '':
+                #   Hour                
                 hora = datetime.datetime.now()
-                print(str(hora)+ ' ' + m.replace('\n', '')+"\n")
-                tempList += [str(m.replace('\n', '')) + ' - envida\n']           
-        reset  = open("chat.txt", "w")
-        reset.writelines(tempList)
+                finalTime = str(hora).split('.')
+                nowUser = str(formtMsg.split(':')[0])
+                
+                #   Print, validete & lastUser
+                if nowUser != lastUser:
+                    print('\n' + finalTime[0] + ' ' + m.replace('\n', ''))
+                else:
+                    print(finalTime[0] + ' ' + m.replace('\n', ''))
+
+                tempList += [str(m.replace('\n', '')) + ' - &$#@24!%@\n']   
+                lastUser = formtMsg.split(':')[0] 
+
+                #   Reset       
+                reset = open("chat.txt", "w")
+                reset.writelines(tempList)         
 
 def regUser():
     print("\n------------------- CADASTRO US -------------------\n")
     nameUS  = str(input("Nome: "))
-    user = Usuario(nameUS)
-    print(" > Usuário cadastrado com sucesso, bom proveito!\n")
+    user = User(nameUS)
+    print("\n > Usuário cadastrado com sucesso, bom proveito!\n")
+    print("\n---------------------------------------------------\n")
     return user
 
 def cadMsg(user, msg):
-    arquivo = open("chat.txt", "a")
-    arquivo.write(user.nome + ": " + msg + "\n")
+    setAqr = open("chat.txt", "a")
+    setAqr.write(user.name + ": " + msg + "\n")
 
 ####    Main
-print("\n--------------- BEM-VINDO AO CHAT ---------------\n")
+print("\n---------------- BEM-VINDO AO CHAT ----------------")
 
-choise  = str(input("Deseja iniciar a tela de bate-papo? (S/n) ")).upper()
+choise  = str(input("\nDeseja iniciar a tela de bate-papo? (S/n) -> ")).upper()
 
 if choise != "S":
     user = regUser()
 else:
-    print(" > Chat iniciado com sucesso, bom proveito!\n")
+    print("\n > Chat iniciado com sucesso, bom proveito!")
+    print("\n---------------------------------------------------")
     chatShow()
 
 while True:
     msg = str(input("Texto: "))
     if msg == "./":
         break
-
     cadMsg(user, msg)
 
 print("\n------ CHAT FINALIZADO ------\n")
